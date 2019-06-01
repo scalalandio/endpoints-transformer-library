1260f49252a4c9e00ec7143f78d625cfbc54b57c
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2019-06-01 15:30:02 +0200

    Cleanup

--- scripts/.common.sh
+++ scripts/.common.sh
@@ -1,8 +1,5 @@
 export ScriptsRel=`basename "${0:a:h}"`
-export TmuxInitDir=`dirname "${0:a:h}"`
-export TmuxSessionName='Endpointstransformerlibrary'
 
 setopt IGNORE_EOF
 
 source "${0:a:h}/.sbt-helpers.sh"
-source "${0:a:h}/.tmux-helpers.sh"
